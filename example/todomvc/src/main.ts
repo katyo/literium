09b1bc921622cedd21cd93e8dec895bab87c6a72
Kayo Phoenix <kayo@illumium.org>
2018-03-06 18:21:26 +0500

    Added sidebar for todomvc

--- example/todomvc/src/main.ts
+++ example/todomvc/src/main.ts
@@ -1,4 +1,4 @@
-import { Send, Fork, Component, h, fork_map, send_map } from 'literium/types';
+import { VNode, Send, Fork, Component, h, fork_map, send_map } from 'literium/types';
 import { page } from 'literium/page';
 import { todo as todoComponent, State as TodoState, Event as TodoEvent } from './todo';
 
@@ -42,21 +42,82 @@ function render(state: State, send: Send<Event>) {
         styles,
         scripts,
         title: "TodoMVC",
+        body: { class: { 'learn-bar': true } },
     }, [
+            learn(),
             todoComponent.render(state.todo, send_map(send, wrapTodo)),
-            h('footer.info', [
-                h('p', 'Double-click to edit a todo'),
-                h('p', [
-                    'Created by ',
-                    h('a', { attrs: { href: 'https://github.com/katyo' } }, 'Kayo'),
-                ]),
-                h('p', [
-                    'Not a part of ',
-                    h('a', { attrs: { href: 'http://todomvc.com' } }, 'TodoMVC'),
-                    ' yet now.'
-                ]),
-            ]),
+            footer(),
         ]);
 }
 
 export const main: Component<State, Event> = { create, update, render };
+
+interface SourceLink {
+    title: string;
+    url: string;
+    local?: true;
+    kind?: string;
+}
+
+interface SourceLinksGroup {
+    title: string;
+    links: SourceLink[];
+}
+
+const source_links: SourceLinksGroup[] = [
+    {
+        title: 'Example',
+        links: [
+            {
+                title: 'Source',
+                url: 'https://github.com/katyo/literium/tree/master/example/todomvc',
+            }
+        ]
+    }
+];
+
+function learn() {
+    return h('aside.learn', [
+        h('header', [
+            h('h3', 'Literium'),
+            h('span.source-links',
+                source_links.reduce((list, { title, links }) => (list.push(h('h5', title)),
+                    links.forEach(({ title, url, kind, local }) => {
+                        if ((list[list.length - 1] as VNode).sel == 'a') list.push(', ');
+                        list.push(h('a' + (kind ? `.${kind}-link` : ''), {
+                            attrs: {
+                                href: url,
+                                'data-type': local ? 'local' : false
+                            }
+                        }, title));
+                    }), list), [] as (VNode | string)[]))
+        ]),
+        h('hr'),
+        h('blockquote.quote.speech-bubble', [
+            h('p', 'Literium is a client-side framework for modern Web-application. Its core principles are explicit state, controllable behavior, declarative code, effeciency, simplicity and flexibility.'),
+        ]),
+        h('footer', [
+            h('hr'),
+            h('em', [
+                'If you found unexpected behavior in example, or you have something helpful ideas, please ',
+                h('a', { attrs: { href: 'https://github.com/katyo/literium/issues' } }, 'let me know'),
+                '.'
+            ])
+        ])
+    ]);
+}
+
+function footer() {
+    return h('footer.info', [
+        h('p', 'Double-click to edit a todo'),
+        h('p', [
+            'Created by ',
+            h('a', { attrs: { href: 'https://github.com/katyo' } }, 'Kayo'),
+        ]),
+        h('p', [
+            'Not a part of ',
+            h('a', { attrs: { href: 'http://todomvc.com' } }, 'TodoMVC'),
+            ' yet now.'
+        ]),
+    ]);
+}
