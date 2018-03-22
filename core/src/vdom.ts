22839d7bb83a7830a703ff96030a93d1debde4d8
Kayo Phoenix <kayo@illumium.org>
2018-03-22 15:08:10 +0500

    Updated snabbdom-ng dependency

--- core/src/vdom.ts
+++ core/src/vdom.ts
@@ -1,10 +1,10 @@
 import {
     VNode as VNode_, VNodeChild as VNodeChild_, VBaseData, VHooksData, VKey,
-    H, h as h_, VNodeChildren as VNodeChildren_,
+    H, h as h_, VNodeChildren as VNodeChildren_, emptyVData,
     VAttrsData, VPropsData, VStyleData, VClassData, VEventData
 } from 'snabbdom-ng';
 
-export interface VData extends VBaseData, VHooksData<VData>, VAttrsData, VPropsData, VClassData, VStyleData, VEventData { }
+export interface VData extends VBaseData, VHooksData<VData>, VAttrsData, VPropsData, VClassData, VStyleData, VEventData<VData> { }
 
 export const h: H<VData> = h_;
 export type VNode = VNode_<VData>;
@@ -12,7 +12,7 @@ export type VNodeChild = VNodeChild_<VData>;
 export type VNodeChildren = VNodeChildren_<VData>;
 export { VKey };
 
-export const empty: VData = {};
+export const empty: VData = emptyVData;
 
 export function with_key(key: VKey, vnode: VNodeChild): VNodeChild {
     if (vnode != null && typeof vnode == 'object' && vnode.sel) {
