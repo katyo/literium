459fa415c132588e97965fc5e8791ea70b2b715f
Kayo Phoenix <kayo@illumium.org>
2018-06-03 16:29:22 +0500

    Using type-safe JSON module for local storage

--- runner/src/store.ts
+++ runner/src/store.ts
@@ -1,4 +1,5 @@
-import { dummy } from 'literium';
+import { dummy, is_ok, un_ok } from 'literium';
+import { JsonType, parse, build } from 'literium-json';
 
 export interface StoreData<Type> {
     $?: boolean; // data is persistent
@@ -8,46 +9,46 @@ export interface StoreData<Type> {
 export interface StoreCell<Type> {
     $: string; // data cell name
     _: Type; // default value
+    j: JsonType<Type>; // validator
 }
 
 const [_load, _save]: [
-    <Type>(name: string) => StoreData<Type> | void,
-    <Type>(name: string, data: StoreData<Type>) => void
+    <Type>(name: string, json: JsonType<Type>) => StoreData<Type> | void,
+    <Type>(name: string, json: JsonType<Type>, data: StoreData<Type>) => void
 ] = check() ? [load_, save_] : [dummy, dummy];
 
-export function initStore<Type>(name: string, def: Type): StoreCell<Type> {
-    return { $: name, _: def };
+export function initStore<Type, JType extends Type>(name: string, json: JsonType<JType>, def: Type): StoreCell<Type> {
+    return { $: name, _: def, j: json };
 }
 
 export function loadStore<Type>(store: StoreCell<Type>): Type {
-    const cell = _load<Type>(store.$);
+    const cell = _load(store.$, store.j);
     return cell ? cell._ as Type : store._;
 }
 
 export function saveStore<Type>(store: StoreCell<Type>, data?: Type, persist?: boolean) {
-    const cell = _load<Type>(store.$) || { _: store._ };
+    const cell = _load(store.$, store.j) || { _: store._ };
     if (persist != undefined) cell.$ = persist;
     cell._ = data;
-    _save(store.$, cell);
+    _save(store.$, store.j, cell);
 }
 
-function load_<Type>(name: string): StoreData<Type> | void {
+function load_<Type>(name: string, json: JsonType<Type>): StoreData<Type> | void {
     const raw1 = sessionStorage.getItem(name);
     const raw2 = localStorage.getItem(name);
     const raw = raw1 || raw2;
     if (!raw) return;
     const persist = !raw1 && !!raw2;
-    try {
-        return { $: persist, _: JSON.parse(raw) as Type };
-    } catch (_) { }
+    const res = parse(json, raw);
+    return is_ok(res) ? { $: persist, _: un_ok(res) } : undefined;
 }
 
-function save_<Type>(name: string, { $: persist, _: data }: StoreData<Type>) {
+function save_<Type>(name: string, json: JsonType<Type>, { $: persist, _: data }: StoreData<Type>) {
     sessionStorage.removeItem(name);
     localStorage.removeItem(name);
     if (persist != undefined && data != undefined) {
-        const json = JSON.stringify(data);
-        (persist ? localStorage : sessionStorage).setItem(name, json);
+        const res = build(json, data);
+        if (is_ok(res)) (persist ? localStorage : sessionStorage).setItem(name, un_ok(res));
     }
 }
 
