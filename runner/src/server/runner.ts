e16b86d8830ce2fc3ca2dc807821805d3d75da15
Kayo Phoenix <kayo@illumium.org>
2018-07-10 17:13:42 +0500

    Added state to server's runner callback

--- runner/src/server/runner.ts
+++ runner/src/server/runner.ts
@@ -11,29 +11,33 @@ import { VNode, VData, Component } from 'literium';
 import { fork_pool } from '../sched';
 
 export interface Run<State, Signal> {
-    (app: Component<State, Signal>, end: (html: string) => void): void;
+    (app: Component<State, Signal>, end: (html: string, state: State) => void): void;
 }
 
-export function init<State, Signal>(doctype: string = 'html', timeout: number = 1000): Run<State, Signal> {
+export function init<State, Signal>(doctype: string = 'html', timeout?: number): Run<State, Signal> {
     const { read, patch } = init_<VData>([
         attrsModule(attrsApi),
         classModule(classApi),
         styleModule(styleApi),
     ], htmlDomApi);
 
-    return ({ create, update, render }, end: (html: string) => void) => {
+    return ({ create, update, render }, end: (html: string, state: State) => void) => {
         const emit = (event: Signal) => {
             //console.log('emit: ', event);
             state = update(state, event, fork);
         };
-        const [fork, run] = fork_pool(emit, () => {
+        let timer: any;
+        const done = () => {
             //console.log('done');
+            if (timer) clearTimeout(timer);
             const doc = htmlDomApi.createElement('html');
             const vnode = render(state, emit) as VNode;
             patch(read(doc), vnode);
-            end(`<!DOCTYPE ${doctype}>${toHtml(doc)}`);
-        });
+            end(`<!DOCTYPE ${doctype}>${toHtml(doc)}`, state);
+        };
+        const [fork, run] = fork_pool(emit, done);
         let state = create(fork);
+        if (timeout) timer = setTimeout(done, timeout);
         run();
     };
 }
