a1f0757e93ec0bdf713679790f0f38a65be6f3fc
Kayo Phoenix <kayo@illumium.org>
2018-07-12 14:06:50 +0500

    Added convertor node-like async functions to futures

--- base/src/future_result.ts
+++ base/src/future_result.ts
@@ -1,3 +1,4 @@
+import { Emit } from './emit';
 import { Result, ok, err, is_ok, un_ok, un_err } from './result';
 import { Future, future, then_future, FutureConv } from './future';
 
@@ -27,4 +28,33 @@ export function map_future_ok<Value, NewValue>(fn: (v: Value) => NewValue): <Err
 
 export function map_future_err<Error, NewError>(fn: (e: Error) => NewError): <Value>(future: FutureResult<Value, Error>) => FutureResult<Value, NewError> {
     return then_future(<Value>(r: Result<Value, Error>) => is_ok(r) ? future(r as Result<Value, NewError>) : future_err(fn(un_err(r))));
+}
+
+export function future_async<T, E>(fn: (cb: (error?: E, value?: T) => void) => void): FutureResult<T, E>;
+export function future_async<T, E, T1>(fn: (a1: T1, cb: (error?: E, value?: T) => void) => void, a1: T1): FutureResult<T, E>;
+export function future_async<T, E, T1, T2>(fn: (a1: T1, a2: T2, cb: (error?: E, value?: T) => void) => void, a1: T1, a2: T2): FutureResult<T, E>;
+export function future_async<T, E, T1, T2, T3>(fn: (a1: T1, a2: T2, a3: T3, cb: (error?: E, value?: T) => void) => void, a1: T1, a2: T2, a3: T3): FutureResult<T, E>;
+export function future_async<T, E, T1, T2, T3, T4>(fn: (a1: T1, a2: T2, a3: T3, a4: T4, cb: (error?: E, value?: T) => void) => void, a1: T1, a2: T2, a3: T3, a4: T4): FutureResult<T, E>;
+export function future_async<T, E, T1, T2, T3, T4, T5>(fn: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, cb: (error?: E, value?: T) => void) => void, a1: T1, a2: T2, a3: T3, a4: T4, a5: T5): FutureResult<T, E>;
+export function future_async<T, E, T1, T2, T3, T4, T5, T6>(fn: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, cb: (error?: E, value?: T) => void) => void, a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6): FutureResult<T, E>;
+export function future_async<T, E, T1, T2, T3, T4, T5, T6, T7>(fn: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7, cb: (error?: E, value?: T) => void) => void, a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7): FutureResult<T, E>;
+export function future_async<T, E, T1, T2, T3, T4, T5, T6, T7, T8>(fn: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7, a8: T8, cb: (error?: E, value?: T) => void) => void, a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7, a8: T8): FutureResult<T, E>;
+export function future_async<T, E, T1, T2, T3, T4, T5, T6, T7, T8, T9>(fn: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7, a8: T8, a9: T9, cb: (error?: E, value?: T) => void) => void, a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7, a8: T8, a9: T9): FutureResult<T, E>;
+export function future_async<T, E>(fn: (...a: any[]) => void, ...a: any[]): FutureResult<T, E> {
+    return (emit: Emit<Result<T, E>>) => {
+        let final = false;
+        fn(...a, (error: E, value: T) => {
+            if (!final) {
+                final = true;
+                if (error) {
+                    emit(err(error));
+                } else {
+                    emit(ok(value));
+                }
+            }
+        });
+        return () => {
+            final = true;
+        };
+    };
 }
