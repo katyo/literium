759caa4282346622016de8376a4adb16b8ba4393
Kayo Phoenix <kayo@illumium.org>
2018-07-20 01:40:07 +0500

    Added langcode parameter for locale to allow typesafe localization

--- runner/src/language.ts
+++ runner/src/language.ts
@@ -1,9 +1,15 @@
-export interface Locale {
-    lang: string;
+export interface Locale<Lang extends string> {
+    lang: Lang;
     title: string;
 }
 
-export function getLang(locales: Locale[], langs: ReadonlyArray<string>): string {
+export type LocaleLang<L> = L extends Locale<infer Lang> ? Lang : L extends Locale<infer Lang>[] ? Lang : never;
+
+export function mkLocale<Lang extends string>(lang: Lang, title: string): Locale<Lang> {
+    return { lang, title };
+}
+
+export function getLang<Lang extends string>(locales: Locale<Lang>[], langs: ReadonlyArray<string>): Lang {
     for (const lang of langs) {
         for (const { lang: code } of locales) {
             if (lang.slice(0, code.length) == code) {
@@ -11,15 +17,16 @@ export function getLang(locales: Locale[], langs: ReadonlyArray<string>): string
             }
         }
     }
-    return locales.length ? locales[0].lang : '';
+    if (locales.length) return locales[0].lang;
+    throw 'Missing locales';
 }
 
-export interface HasLocales {
-    locales: Locale[];
+export interface HasLocales<Lang extends string> {
+    locales: Locale<Lang>[];
 }
 
-export interface HasLang {
-    lang: string;
+export interface HasLang<Lang extends string> {
+    lang: Lang;
 }
 
 export interface SetLang {
@@ -27,6 +34,6 @@ export interface SetLang {
     lang: string;
 }
 
-export function setLang<State extends HasLang>(state: State, { lang }: SetLang): State {
+export function setLang<Lang extends string, State extends HasLang<Lang>>(state: State, { lang }: SetLang): State {
     return { ...(state as {}), lang } as State;
 }
