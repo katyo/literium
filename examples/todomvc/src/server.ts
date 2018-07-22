1216b8dbf437d99a528c0d6245ccd1eedf4f996b
K <kayo@illumium.org>
2018-07-22 09:55:54 +0500

    Renamed `backend' module to `node'

--- examples/todomvc/src/server.ts
+++ examples/todomvc/src/server.ts
@@ -1,5 +1,5 @@
 import { constant } from 'literium';
-import { resource_handler, runner_handler, http_server, handle_all } from 'literium-backend';
+import { resource_handler, runner_handler, http_server, handle_all } from 'literium-node';
 import { main } from './main';
 
 http_server(handle_all(
