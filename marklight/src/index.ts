ab0a10897213c926c196c79122ca44b958f0c79b
Kayo Phoenix <kayo@illumium.org>
2018-08-23 23:21:39 +0500

    Added simple module with code highlight render rules for markup

---
+++ marklight/src/index.ts
@@ -0,0 +1,41 @@
+import { h } from 'literium';
+import {
+    ContextTag,
+    BlockTag,
+    BlockCode,
+    InlineTag,
+    InlineCode,
+    NoMeta,
+    BlockRenderRuleVDom,
+    InlineRenderRuleVDom
+} from 'literium-markup';
+import {
+    Options,
+    initHighlight
+} from 'literium-highlight';
+
+export function CodeBlockVDom(options?: Options): BlockRenderRuleVDom<BlockCode, NoMeta> {
+    const highlight = initHighlight(options);
+    return [
+        ContextTag.Block,
+        BlockTag.Code,
+        ({ }, { _, l }) => h('pre', h('code', highlight(_, l)))
+    ];
+}
+
+export function CodeBlockWithClassVDom(options?: Options): BlockRenderRuleVDom<BlockCode, NoMeta> {
+    const highlight = initHighlight(options);
+    return [
+        ContextTag.Block,
+        BlockTag.Code,
+        ({ }, { _, l }) => h('pre', h('code', { class: { [`language-${l}`]: !!l } }, highlight(_, l)))
+    ];
+}
+
+export function CodeSpanVDom(options?: Options): InlineRenderRuleVDom<InlineCode, NoMeta> {
+    return [
+        ContextTag.Inline,
+        InlineTag.Code,
+        ({ }, { _ }) => h('code', _)
+    ];
+}
