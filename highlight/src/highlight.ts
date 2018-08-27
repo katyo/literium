499f62582f6088a4455abf2e578ab3287cc15b9f
Kayo Phoenix <kayo@illumium.org>
2018-08-28 00:29:45 +0500

    Prevent highlighter faults by removing unsupported languages

--- highlight/src/highlight.ts
+++ highlight/src/highlight.ts
@@ -1,5 +1,5 @@
 import { VNodeChildren } from '@literium/core';
-import { Options, init, process } from 'highlight-ts';
+import { Options, listLanguages, init, process } from 'highlight-ts';
 import { vdomRender } from './vdom';
 
 export interface Highlight {
@@ -10,6 +10,15 @@ export function initHighlight(options?: Options): Highlight {
     const highlighter = init(vdomRender, options);
 
     return (src, lang) => {
+        if (lang) {
+            const langs = listLanguages();
+            // remove unsupported languages to prevent fault
+            if (typeof lang == 'string') {
+                if (langs.indexOf(lang) < 0) lang = undefined;
+            } else {
+                lang = lang.filter(lang => langs.indexOf(lang) >= 0);
+            }
+        }
         const result = process(highlighter, src, lang);
         return result.value;
     };
