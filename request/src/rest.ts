9d938527848cac2227a71ac2bf736082ea2bfb9c
Kayo Phoenix <kayo@illumium.org>
2018-12-04 16:17:49 +0500

    Fixed request REST types

--- request/src/rest.ts
+++ request/src/rest.ts
@@ -6,7 +6,7 @@ import { method_use, origin_from, status_exact } from './plugins';
 
 export type HasBase = HasField<'base', string>;
 
-export function rest_get<U, A extends HasBase, R>(req_route: Route<U>, res_body: Plugin<A, R>): Plugin<U & A, R> {
+export function rest_get<U, A, R>(req_route: Route<U>, res_body: Plugin<A, R>): Plugin<U & A & HasBase, R> {
     return mix_plugin(
         method_use(Method.Get),
         origin_from()('base'),
@@ -16,11 +16,11 @@ export function rest_get<U, A extends HasBase, R>(req_route: Route<U>, res_body:
     );
 }
 
-export function rest_post<U, A extends HasBase, R>(req_route: Route<U>, req_body: Plugin<A, R>): Plugin<U & A, R>;
-export function rest_post<U, A extends HasBase, R, L>(req_route: Route<U>, req_body: Plugin<A, R>, res_route: Route<L>): Plugin<U & A, L & R>;
-export function rest_post<U, A extends HasBase, R, L, B, T>(req_route: Route<U>, req_body: Plugin<A, R>, res_route: Route<L>, res_body: Plugin<B, T>): Plugin<U & A, L & R & T>;
-export function rest_post<U, A extends HasBase, R, B, T>(req_route: Route<U>, req_body: Plugin<A, R>, res_body: Plugin<B, T>): Plugin<U & A, R & T>;
-export function rest_post<U, A extends HasBase, R, L, B, T>(req_route: Route<U>, req_body: Plugin<A, R>, res_route?: Route<L> | Plugin<B, T>, res_body?: Plugin<B, T>): Plugin<U & A & B, L & R & T> {
+export function rest_post<U, A, R>(req_route: Route<U>, req_body: Plugin<A, R>): Plugin<U & A & HasBase, R>;
+export function rest_post<U, A, R, L>(req_route: Route<U>, req_body: Plugin<A, R>, res_route: Route<L>): Plugin<U & A & HasBase, L & R>;
+export function rest_post<U, A, R, L, B, T>(req_route: Route<U>, req_body: Plugin<A, R>, res_route: Route<L>, res_body: Plugin<B, T>): Plugin<U & A & B & HasBase, L & R & T>;
+export function rest_post<U, A, R, B, T>(req_route: Route<U>, req_body: Plugin<A, R>, res_body: Plugin<B, T>): Plugin<U & A & B & HasBase, R & T>;
+export function rest_post<U, A, R, L, B, T>(req_route: Route<U>, req_body: Plugin<A, R>, res_route?: Route<L> | Plugin<B, T>, res_body?: Plugin<B, T>): Plugin<U & A & B & HasBase, L & R & T> {
     if (typeof res_route == 'function') {
         res_body = res_route as Plugin<B, T>;
         res_route = undefined;
@@ -36,9 +36,9 @@ export function rest_post<U, A extends HasBase, R, L, B, T>(req_route: Route<U>,
     );
 }
 
-export function rest_put<U, A extends HasBase, R>(req_route: Route<U>, req_body: Plugin<A, R>): Plugin<U & A, R>;
-export function rest_put<U, A extends HasBase, R, B, T>(req_route: Route<U>, req_body: Plugin<A, R>, res_body: Plugin<B, T>): Plugin<U & A, R & T>;
-export function rest_put<U, A extends HasBase, R, B, T>(req_route: Route<U>, req_body: Plugin<A, R>, res_body?: Plugin<B, T>): Plugin<U & A & B, R & T> {
+export function rest_put<U, A, R>(req_route: Route<U>, req_body: Plugin<A, R>): Plugin<U & A & HasBase, R>;
+export function rest_put<U, A, R, B, T>(req_route: Route<U>, req_body: Plugin<A, R>, res_body: Plugin<B, T>): Plugin<U & A & HasBase, R & T>;
+export function rest_put<U, A, R, B, T>(req_route: Route<U>, req_body: Plugin<A, R>, res_body?: Plugin<B, T>): Plugin<U & A & B & HasBase, R & T> {
     return mix_plugin(
         method_use(Method.Put),
         origin_from()('base'),
@@ -49,9 +49,9 @@ export function rest_put<U, A extends HasBase, R, B, T>(req_route: Route<U>, req
     );
 }
 
-export function rest_delete<U, A extends HasBase, R>(req_route: Route<U>): Plugin<U & A, R>;
-export function rest_delete<U, A extends HasBase, R, B, T>(req_route: Route<U>, res_body: Plugin<B, T>): Plugin<U & A, R & T>;
-export function rest_delete<U, A extends HasBase, R, B, T>(req_route: Route<U>, res_body?: Plugin<B, T>): Plugin<U & A & B, R & T> {
+export function rest_delete<U, A, R>(req_route: Route<U>): Plugin<U & A & HasBase, R>;
+export function rest_delete<U, A, R, B, T>(req_route: Route<U>, res_body: Plugin<B, T>): Plugin<U & A & HasBase, R & T>;
+export function rest_delete<U, A, R, B, T>(req_route: Route<U>, res_body?: Plugin<B, T>): Plugin<U & A & B & HasBase, R & T> {
     return mix_plugin(
         method_use(Method.Delete),
         origin_from()('base'),
@@ -61,7 +61,7 @@ export function rest_delete<U, A extends HasBase, R, B, T>(req_route: Route<U>, 
     );
 }
 
-export function rest_head<U, A extends HasBase, R>(req_route: Route<U>): Plugin<U & A, R> {
+export function rest_head<U, A, R>(req_route: Route<U>): Plugin<U & A & HasBase, R> {
     return mix_plugin(
         method_use(Method.Head),
         origin_from()('base'),
